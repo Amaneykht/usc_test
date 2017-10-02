824b192368634de2f41e19008dabc94d69080212
Amaney Khayyat <amaney.it@gmail.com>
2017-10-02 09:20:47 +0300

    Fixes

--- models/Drawing.php
+++ models/Drawing.php
@@ -5,13 +5,21 @@
  */
 
 namespace models;
+use models\drawing\IDrawable;
 
 /**
+ * This class is responsible of drawing, it takes any drawble object and draw it.
+ * Using this structure we can add other implementation other than shapes, we can add Images, Text ..etc
+ * we can use the creator for each components
+ *
  * Class Drawing
  * @package models
  */
 class Drawing
 {
+    /**
+     * @var IDrawable
+     */
     private $_drawable;
 
     public function __construct($drawable)
